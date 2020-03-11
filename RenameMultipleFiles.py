c6dee8056e1a7abe4a0b95515d16523bf3370ffa
Vinay S <vinayakumarvs@users.noreply.github.com>
2020-03-11 16:14:18 +0530

    Update RenameMultipleFiles.py

--- RenameMultipleFiles.py
+++ RenameMultipleFiles.py
@@ -1,9 +1,12 @@
+from ..imports import *
+
+
 # Function to rename multiple files 
 def main(): 
     i = 0
       
-    for filename in os.listdir("."):
-        f_name, e_name = os.path.splitext(filename)
-        dst ="Image" + str(i) + e_name
-        os.rename(filename,dst)
-        i += 1
+    for filename in os.listdir("."): #List all the files from the working directory
+        f_name, e_name = os.path.splitext(filename) #Split filename and the extension
+        dst ="Image" + str(i) + e_name #Destination filename
+        os.rename(filename,dst) #Rename the file
+        i += 1  #Increment the file numbers
